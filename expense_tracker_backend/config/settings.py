a68365d2115e8d20fbf95dc4274549d98168477a
Elutayo <elutayojohnson@gmail.com>
2026-07-31 01:09:27 +0100

    add requirement for deployment

--- expense_tracker_backend/config/settings.py
+++ expense_tracker_backend/config/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%%--i!gu_ln!x%fp7#^+%gsu_m%ge%iw^s@h&cu4fp4903!6hr'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'false'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
