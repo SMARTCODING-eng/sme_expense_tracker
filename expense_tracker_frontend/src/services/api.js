b1b933dfce3984f0185b3d369348992808372f4e
Elutayo <elutayojohnson@gmail.com>
2026-08-04 23:55:53 +0100

    bbb

--- expense_tracker_frontend/src/services/api.js
+++ expense_tracker_frontend/src/services/api.js
@@ -2,10 +2,8 @@
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
 
-
-console.log("API_BASE_URL =", API_BASE_URL);
-console.log("ENV =", import.meta.env);
-
+console.log(import.meta.env.VITE_API_BASE_URL);
+console.log(import.meta.env);
 /**
  * Format JS transaction object into Django REST Framework expected payload
  */
