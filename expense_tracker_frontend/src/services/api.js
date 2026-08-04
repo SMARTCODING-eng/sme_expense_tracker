6d767ae2bd8808619f2b742d5d1e04b2a271ab24
Elutayo <elutayojohnson@gmail.com>
2026-08-04 23:49:45 +0100

    bbb

--- expense_tracker_frontend/src/services/api.js
+++ expense_tracker_frontend/src/services/api.js
@@ -1,6 +1,10 @@
 // API service layer for connecting React frontend to Django REST Framework backend
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
+
+
+console.log("API_BASE_URL =", API_BASE_URL);
+console.log("ENV =", import.meta.env);
 
 /**
  * Format JS transaction object into Django REST Framework expected payload
